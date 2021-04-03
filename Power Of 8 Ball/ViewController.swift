36dc192629222811df5a778aa149e75bb552bd33
ATIN AGNIHOTRI <atin.agnihotri@paytmmoney.com>
2021-04-03 12:59:08 +0530

    Initial Commit

--- Power Of 8 Ball/ViewController.swift
+++ Power Of 8 Ball/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var eightBallImageView: UIImageView!
+    
+    let eightBallArray = [#imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball5"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball3")]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        print("Ask Button Pressed")
+        eightBallImageView.image = eightBallArray.randomElement()
+        
+    }
+    
 }
 
